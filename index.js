01e79bc5adf88100eaf6f57efbee775b7760d8a2
antoninbrejcha <brejcha.an.2023@skola.ssps.cz>
2025-01-20 15:57:17 +0100

    implement canvas setup and basic tetromino functionality

--- index.js
+++ index.js
@@ -1,1 +1,134 @@
 let canvas;
+let ctx;
+let gBArrayHeight = 20;
+let gBArrayWidth = 12;
+let startX = 4;
+let stastY = 0;
+let coordinateArray = [...Array(gBArrayHeight)].map((e) =>
+  Array(gBArrayWidth).fill(0)
+);
+let curTetromino = [
+  [1, 0],
+  [0, 1],
+  [1, 1],
+  [2, 1],
+];
+
+let tetrominos = [];
+let tetrominoColors = [
+  "purple",
+  "cyan",
+  "blue",
+  "yellow",
+  "orange",
+  "green",
+  "red",
+];
+let curTetrominoColor;
+let gameBoardArray = [...Array(gBArrayHeight)].map((e) =>
+  Array(gBArrayWidth).fill(0)
+);
+
+let DIRECTON = {
+  IDLE: 0,
+  DOWN: 1,
+  LEFT: 2,
+  RIGHT: 3,
+};
+class Coordinates {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", SetupCanvas);
+
+function CreateCoordArray() {
+  let i = 0,
+    j = 0;
+  for (let y = 9; y <= 446; y += 23) {
+    for (let x = 11; x <= 264; x += 23) {
+      coordinateArray[i][j] = new Coordinates(x, y);
+      i++;
+    }
+    j++;
+    i = 0;
+  }
+}
+
+function SetupCanvas() {
+  canvas = document.getElementById("my-canvas");
+  ctx = canvas.getContext("2d");
+  canvas.width = 936;
+  canvas.height = 956;
+  ctx.scale(2, 2);
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.strokeStyle = "black";
+  ctx.strokeRect(8, 8, 280, 462);
+  document.addEventListener("keydown", HandleKeyPress);
+  CreateTetrominos();
+  CreateTetromino();
+  CreateCoordArray();
+  DrawTetromino();
+}
+
+function DrawTetromino() {
+  for (let i = 0; i < curTetromino.length; i++) {
+    let x = curTetromino[i][0] + startX;
+    let y = curTetromino[i][1] + stastY;
+    gameBoardArray[x][y] = 1;
+    let coorX = coordinateArray[x][y].x;
+    let coorY = coordinateArray[x][y].y;
+    ctx.fillStyle = curTetrominoColor;
+    ctx.fillRect(coorX, coorY, 21, 21);
+  }
+}
+
+function HandleKeyPress(key) {
+  if (key.keyCode === 65) {
+    direction = DIRECTON.LEFT;
+    DeleteTetromino();
+    startX--;
+    DrawTetromino();
+  } else if (key.keyCode === 68) {
+    direction = DIRECTON.RIGHT;
+    DeleteTetromino();
+    startX++;
+    DrawTetromino();
+  } else if (key.keyCode === 83) {
+    direction = DIRECTON.DOWN;
+    DeleteTetromino();
+    stastY++;
+    DrawTetromino();
+  }
+}
+
+function DeleteTetromino() {
+  for (let i = 0; i < curTetromino.length; i++) {
+    let x = curTetromino[i][0] + startX;
+    let y = curTetromino[i][1] + stastY;
+    gameBoardArray[x][y] = 0;
+    let coorX = coordinateArray[x][y].x;
+    let coorY = coordinateArray[x][y].y;
+    ctx.fillStyle = "white";
+    ctx.fillRect(coorX, coorY, 21, 21);
+  }
+}
+
+function CreateTetrominos() {
+    tetrominos.push([[1, 0], [0, 1], [1, 1], [2, 1]]);
+    tetrominos.push([[0, 0], [1, 0], [2, 0], [3, 0]]);
+    tetrominos.push([[0, 0], [0, 1], [1, 1], [2, 1]]);
+    tetrominos.push([[0, 0], [1, 0], [0, 1], [1, 1]]);
+    tetrominos.push([[1, 0], [2, 0], [0, 1], [1, 1]]);
+    tetrominos.push([[0, 0], [1, 0], [1, 1], [2, 1]]);
+    tetrominos.push([[2, 0], [0, 1], [1, 1], [2, 1]]);
+}
+
+function CreateTetromino() {
+    let randomTetromino = Math.floor(Math.random() * tetrominos.length);
+    curTetromino = tetrominos[randomTetromino];
+    curTetrominoColor = tetrominoColors[randomTetromino];
+}
