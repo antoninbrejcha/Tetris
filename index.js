ea3c56cc4ce2c2c9f12b9d5bda58794e05aff197
antoninbrejcha <brejcha.an.2023@skola.ssps.cz>
2025-03-02 16:08:36 +0100

    drawing the console

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ let ctx;
 let gBArrayHeight = 20;
 let gBArrayWidth = 12;
 let startX = 4;
-let stastY = 0;
-let score = 0;
-let level = 1;
-let winOrLose = "Playing";
+let startY = 0;
 let coordinateArray = [...Array(gBArrayHeight)].map((e) =>
   Array(gBArrayWidth).fill(0)
 );
+
 let curTetromino = [
   [1, 0],
   [0, 1],
@@ -17,34 +15,11 @@ let curTetromino = [
   [2, 1],
 ];
 
-let tetrominos = [];
-let tetrominoColors = [
-  "purple",
-  "cyan",
-  "blue",
-  "yellow",
-  "orange",
-  "green",
-  "red",
-];
-let curTetrominoColor;
-let gameBoardArray = [...Array(gBArrayHeight)].map((e) =>
-  Array(gBArrayWidth).fill(0)
-);
-let stoppedShapeArray = [...Array(gBArrayHeight)].map((e) =>
-  Array(gBArrayWidth).fill(0)
-);
-
-let DIRECTON = {
-  IDLE: 0,
-  DOWN: 1,
-  LEFT: 2,
-  RIGHT: 3,
-};
 class Coordinates {
   constructor(x, y) {
     this.x = x;
     this.y = y;
+    CreateCoordArray();
   }
 }
 
@@ -66,130 +41,19 @@ function CreateCoordArray() {
 function SetupCanvas() {
   canvas = document.getElementById("my-canvas");
   ctx = canvas.getContext("2d");
-  canvas.width = 936;
-  canvas.height = 956;
-  ctx.scale(2, 2);
-  ctx.fillStyle = "white";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.strokeStyle = "black";
-  ctx.strokeRect(8, 8, 280, 462);
+  /*let img = document.getElementById("console");
+  canvas.width = img.width;
+  canvas.height = img.height;
+  ctx.scale(0.5, 0.5);
+  ctx.drawImage(img, 0, 0);
+  ctx.strokeRect(0, 0, img.width, img.height);
+  ctx.beginPath();
+  ctx.arc(canvas.width / 2, canvas.height / 2 - 200, 50, 0, Math.PI * 2);
+  ctx.fillStyle = "red";
+  ctx.fill();*/
 
-  tetrisLogo = new Image(161, 54);
-  tetrisLogo.onload = DrawTetrisLogo;
-  tet
-  document.addEventListener("keydown", HandleKeyPress);
-  CreateTetrominos();
-  CreateTetromino();
-  CreateCoordArray();
-  DrawTetromino();
-}
-
-function DrawTetromino() {
-  for (let i = 0; i < curTetromino.length; i++) {
-    let x = curTetromino[i][0] + startX;
-    let y = curTetromino[i][1] + stastY;
-    gameBoardArray[x][y] = 1;
-    let coorX = coordinateArray[x][y].x;
-    let coorY = coordinateArray[x][y].y;
-    ctx.fillStyle = curTetrominoColor;
-    ctx.fillRect(coorX, coorY, 21, 21);
-  }
-}
-
-function HandleKeyPress(key) {
-  if (key.keyCode === 65) {
-    direction = DIRECTON.LEFT;
-    if (!HittingTheWall()) {
-      DeleteTetromino();
-      startX--;
-      DrawTetromino();
-    }
-  } else if (key.keyCode === 68) {
-    direction = DIRECTON.RIGHT;
-    if (!HittingTheWall()) {
-      DeleteTetromino();
-      startX++;
-      DrawTetromino();
-    }
-  } else if (key.keyCode === 83) {
-    direction = DIRECTON.DOWN;
-    DeleteTetromino();
-    stastY++;
-    DrawTetromino();
-  }
-}
-
-function DeleteTetromino() {
-  for (let i = 0; i < curTetromino.length; i++) {
-    let x = curTetromino[i][0] + startX;
-    let y = curTetromino[i][1] + stastY;
-    gameBoardArray[x][y] = 0;
-    let coorX = coordinateArray[x][y].x;
-    let coorY = coordinateArray[x][y].y;
-    ctx.fillStyle = "white";
-    ctx.fillRect(coorX, coorY, 21, 21);
-  }
-}
-
-function CreateTetrominos() {
-  tetrominos.push([
-    [1, 0],
-    [0, 1],
-    [1, 1],
-    [2, 1],
-  ]);
-  tetrominos.push([
-    [0, 0],
-    [1, 0],
-    [2, 0],
-    [3, 0],
-  ]);
-  tetrominos.push([
-    [0, 0],
-    [0, 1],
-    [1, 1],
-    [2, 1],
-  ]);
-  tetrominos.push([
-    [0, 0],
-    [1, 0],
-    [0, 1],
-    [1, 1],
-  ]);
-  tetrominos.push([
-    [1, 0],
-    [2, 0],
-    [0, 1],
-    [1, 1],
-  ]);
-  tetrominos.push([
-    [0, 0],
-    [1, 0],
-    [1, 1],
-    [2, 1],
-  ]);
-  tetrominos.push([
-    [2, 0],
-    [0, 1],
-    [1, 1],
-    [2, 1],
-  ]);
-}
-
-function CreateTetromino() {
-  let randomTetromino = Math.floor(Math.random() * tetrominos.length);
-  curTetromino = tetrominos[randomTetromino];
-  curTetrominoColor = tetrominoColors[randomTetromino];
-}
-
-function HittingTheWall() {
-  for (let i = 0; i < curTetromino.length; i++) {
-    let newX = curTetromino[i][0] + startX;
-    if (newX <= 0 && direction === DIRECTON.LEFT) {
-      return true;
-    } else if (newX >= 11 && direction === DIRECTON.RIGHT) {
-      return true;
-    }
-  }
-  return false;
+  canvas.width = 870;
+  canvas.height = 1500;
+  ctx.fillStyle = "orange";
+  ctx.fillRect(0, 0, 870, 1500);
 }
