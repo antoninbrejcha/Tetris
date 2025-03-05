4011704bc1cae3aa2bc50db14bdaba0a8b64a54b
antoninbrejcha <brejcha.an.2023@skola.ssps.cz>
2025-03-05 11:36:48 +0100

    add tetromino color management and enhance canvas rendering

--- index.js
+++ index.js
@@ -15,6 +15,30 @@ let curTetromino = [
   [2, 1],
 ];
 
+let tetrominos = [];
+let tetrominoColors = [
+  "purple",
+  "cyan",
+  "blue",
+  "yellow",
+  "orange",
+  "green",
+  "red",
+];
+let curTetrominoColor;
+
+let gameBoardArray = [...Array(gBArrayHeight)].map((e) =>
+  Array(gBArrayWidth).fill(0)
+);
+
+let DIRECTION = {
+  IDLE: 0,
+  DOWN: 1,
+  LEFT: 2,
+  RIGHT: 3,
+};
+let direction;
+
 class Coordinates {
   constructor(x, y) {
     this.x = x;
@@ -52,8 +76,99 @@ function SetupCanvas() {
   ctx.fillStyle = "red";
   ctx.fill();*/
 
-  canvas.width = 870;
-  canvas.height = 1500;
-  ctx.fillStyle = "orange";
-  ctx.fillRect(0, 0, 870, 1500);
+  canvas.width = 1000;
+  canvas.height = 1000;
+  roundedRect(ctx, 0, 0, canvas.width, canvas.height, 75, "#ffd60a");
+  displayBorder(ctx, 85, 50, 840, 600, 100, 80, 5, "black");
+  let displayStartX = 250;
+  let displayStartY = 60;
+  let displayWidth = 500;
+  let displayHeight = 500;
+  display(
+    ctx,
+    displayStartX,
+    displayStartY,
+    displayWidth,
+    displayHeight,
+    "white"
+  );
+
+  function roundedRect(ctx, x, y, width, height, radius, color) {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(x + radius, y);
+    ctx.arcTo(x + width, y, x + width, y + radius, radius);
+    ctx.arcTo(x + width, y + height, x + width - radius, y + height, radius);
+    ctx.arcTo(x, y + height, x, y + height - radius, radius);
+    ctx.arcTo(x, y, x + radius, y, radius);
+    ctx.closePath();
+    ctx.fill();
+  }
+
+  function displayBorder(
+    ctx,
+    x,
+    y,
+    width,
+    height,
+    offsetWidth,
+    offsetHeight,
+    radius,
+    color
+  ) {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(x + radius, y);
+    ctx.lineTo(x + width - radius, y);
+    ctx.arcTo(x + width, y, x + width, y + radius, radius);
+    ctx.lineTo(x + width, y + height - radius);
+    ctx.arcTo(x + width, y + height, x + width - radius, y + height, radius);
+    ctx.lineTo(x + width / 2, y + height);
+    ctx.lineTo(x + width / 2 - offsetWidth, y + height - offsetHeight);
+    ctx.lineTo(x + radius, y + height - offsetHeight);
+    ctx.arcTo(
+      x,
+      y + height - offsetHeight,
+      x,
+      y + height - offsetHeight - radius,
+      radius
+    );
+    ctx.lineTo(x, y + radius);
+    ctx.arcTo(x, y, x + radius, y, radius);
+    ctx.closePath();
+    ctx.fill();
+  }
+
+  function display(ctx, x, y, width, height, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, width, height);
+  }
+
+  document.addEventListener("keydown", HandleKeyPress);
+  CreateTetrominos();
+  CreateTetromino();
+
+  CreateCoordArray();
+  DrawTetromino();
+}
+
+function DrawTetromino() {
+  for (let i = 0; i < curTetromino.length; i++) {
+    let x = curTetromino[i][0] + startX;
+    let y = curTetromino[i][1] + startY;
+    gameBoardArray[x][y] = 1;
+    let coorX = coordinateArray[x][y].x;
+    let coorY = coordinateArray[x][y].y;
+    ctx.fillStyle = curTetrominoColor;
+    ctx.fillRect(coorX, coorY, 21, 21);
+  }
+}
+
+function HandleKeyPress(key) {
+  if (key.keyCode === 65) {
+    direction = DIRECTION.LEFT;
+    DeleteTetromino();
+    startX--;
+    DrawTetromino();
+  }
 }
