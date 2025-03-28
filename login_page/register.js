ce0fbdf2a48766131ae1332c43994669062e3213
antoninbrejcha <brejcha.an.2023@skola.ssps.cz>
2025-03-28 14:42:50 +0100

    refactor login and register pages; adjust layout and add email input fields; implement Firebase authentication logic

---
+++ login_page/register.js
@@ -0,0 +1,58 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/11.5.0/firebase-auth.js";
+
+import {
+  getFirestore,
+  doc,
+  setDoc,
+} from "https://www.gstatic.com/firebasejs/11.5.0/firebase-firestore.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCCjbH0oI2ZWDt_15jHoEr-9LaebqcOrts",
+  authDomain: "tetris-ab2dc.firebaseapp.com",
+  projectId: "tetris-ab2dc",
+  storageBucket: "tetris-ab2dc.firebasestorage.app",
+  messagingSenderId: "435753979009",
+  appId: "1:435753979009:web:0b1da3daa7e3e85e9ad180",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const loginRegisterButton = document.getElementById("login-register-button-id");
+
+loginRegisterButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  const username = document.getElementById("username-field").value;
+  const email = document.getElementById("email-field").value;
+  const password = document.getElementById("password-field").value;
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed up
+
+      const user = userCredential.user;
+      return setDoc(doc(db, "users", user.uid), {
+        username: username,
+        email: user.email,
+        uid: user.uid,
+      }).then(() => {
+        alert("User registered successfully!");
+      });
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage);
+      // ..
+    });
+});
